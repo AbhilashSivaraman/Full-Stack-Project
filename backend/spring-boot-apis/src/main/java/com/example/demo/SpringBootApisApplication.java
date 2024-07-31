65c65195a95e53f9ffd8ca9e6ea8ce2c5fa97a21
Abhilash Sivaraman <abhilashsiva1310@gmail.com>
2024-07-31 20:49:02 +0530

    Boot and DB Done

---
+++ backend/spring-boot-apis/src/main/java/com/example/demo/SpringBootApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApisApplication.class, args);
+	}
+
+}
